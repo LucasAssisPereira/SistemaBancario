3248d670af2d69c115a13fba0d6df7223ec51fac
Lucas Assis Pereira <lucasassis1905@gmail.com>
2024-04-27 22:50:24 -0300

    Implementada classe Banco.java

--- src/main/java/entities/Banco.java
+++ src/main/java/entities/Banco.java
@@ -1,4 +1,27 @@
 package entities;
 
 public class Banco {
+    private static Banco instance;
+    private final Conta contaBanco;
+
+    private Banco() {
+        contaBanco = new Conta(0); // O saldo inicial do banco é zero
+    }
+
+    public static synchronized Banco getInstance() {
+        if (instance == null) {
+            instance = new Banco();
+        }
+        return instance;
+    }
+
+    public synchronized void transferir(Conta origem, Conta destino, double valor) {
+        origem.sacar(valor);
+        destino.depositar(valor);
+        System.out.println("Transferência de " + valor + " realizada.");
+    }
+
+    public Conta getContaBanco() {
+        return contaBanco;
+    }
 }
