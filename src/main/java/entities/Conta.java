36e7f32ebfd7f683634d6f69afc092760aefb342
Lucas Assis Pereira <lucasassis1905@gmail.com>
2024-04-27 22:45:37 -0300

    Inserido a classe Conta.java

--- src/main/java/entities/Conta.java
+++ src/main/java/entities/Conta.java
@@ -1,4 +1,35 @@
 package entities;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Conta {
+    private double saldo;
+    private final Lock lock = new ReentrantLock();
+
+    public Conta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        lock.lock();
+        try {
+            saldo += valor;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void sacar(double valor) {
+        lock.lock();
+        try {
+            saldo -= valor;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
